5a941bd3c75400af5accee3a0f6f6d7789263f85
Paul <paul.iand3138@hotmail.com>
2022-12-22 21:32:50 +0800

    Create CartContext and add functionality to add meals into cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,22 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
 
 import classes from "./Cart.module.css";
 
+import CartContext from "../../store/cart-context";
+
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
     const cartItems = (
         <ul className={classes["cart-items"]}>
-            {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
-                (item) => (
-                    <li>{item.name}</li>
-                )
-            )}
+            {cartCtx.items.map((item) => (
+                <li>{item.name}</li>
+            ))}
         </ul>
     );
     return (
@@ -17,7 +24,7 @@ const Cart = (props) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button
@@ -26,7 +33,7 @@ const Cart = (props) => {
                 >
                     Close
                 </button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,24 @@
+import { useContext } from "react";
+
 import MealItemForm from "./MealItemForm";
 
 import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            totalAmount: amount,
+            price: props.price,
+        });
+        console.log(props.price * amount);
+    };
+
     return (
         <li className={classes.meal}>
             <div>
@@ -12,7 +27,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
